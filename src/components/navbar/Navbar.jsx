17cf71e47d20fff1e334f070479d972901daaa33
Ajay Sharma <ajaysh73@gmail.com>
2024-01-03 15:06:37 -0500

    Navbar styling

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Links from '../links/Links';
-
+import styles from './navbar.module.css';
+import { Style_Script } from 'next/font/google';
 const Navbar = () => {
   return (
-    <>
-      <div>logo</div>
+    <div className={styles.container}>
+      <div className={styles.logo}>logo</div>
       <div>
         <Links />
       </div>
-    </>
+    </div>
   );
 };
 
