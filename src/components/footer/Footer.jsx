d6b530ea215a1be7e57c8986871e0def7ee18216
Ajay Sharma <ajaysh73@gmail.com>
2024-01-05 14:04:59 -0500

    Add homepage

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
-
+import styles from './footer.module.css';
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>DevThoughts</div>
+      <div className={styles.text}>Sharma creative thoughts agency All rights reserved.</div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import Links from '../links/Links';
 import styles from './navbar.module.css';
 import { Style_Script } from 'next/font/google';
+import Link from 'next/link';
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>logo</div>
+      <Link href='/' className={styles.logo}>
+        DevThoughts
+      </Link>
       <div>
         <Links />
       </div>
