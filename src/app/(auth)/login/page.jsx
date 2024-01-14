853d9a266aaf74f30b8e0d8b3ec63f295070460b
Ajay Sharma <ajaysh73@gmail.com>
2024-01-14 15:25:01 -0500

    Add Github Authentication

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,7 +1,21 @@
+import { auth, signIn } from '@/lib/auth';
 import React from 'react';
 
-const LoginPage = () => {
-  return <div>LoginPage</div>;
+const LoginPage = async () => {
+  const session = await auth();
+  console.log(session);
+
+  const loginGithub = async () => {
+    'use server';
+    await signIn('github');
+  };
+  return (
+    <div>
+      <form action={loginGithub}>
+        <button>Login using GitHub</button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
