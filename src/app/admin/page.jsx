8f5cd65b542e40e7688f0b18254c42d4c29deb3b
Ajay Sharma <ajaysh73@gmail.com>
2024-01-21 16:02:37 -0500

    Add AdminPost component

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
-
+import styles from './admin.module.css';
+import AdminPosts from '@/components/adminPosts/adminPosts';
 const AdminPage = () => {
-  return <div>AdminPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.row}>
+        <div className={styles.col}>
+          <AdminPosts />
+        </div>
+        <div className={styles.col}> </div>
+      </div>
+      <div className={styles.row}>
+        <div className={styles.col}></div>
+        <div className={styles.col}> </div>
+      </div>
+    </div>
+  );
 };
 
 export default AdminPage;
